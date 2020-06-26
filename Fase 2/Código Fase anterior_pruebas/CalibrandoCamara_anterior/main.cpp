4957fbc160c2b3d07bd6231b796fe4f0063d03c2
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-06-26 17:46:51 -0600

    Update main.cpp

--- Fase 2/Código Fase anterior_pruebas/CalibrandoCamara_anterior/main.cpp
+++ Fase 2/Código Fase anterior_pruebas/CalibrandoCamara_anterior/main.cpp
@@ -1,17 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "opencv2/opencv.hpp"
-
-using namespace cv;
-
-VideoCapture cam2;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    cam2.open(0);
 
     return a.exec();
 }
