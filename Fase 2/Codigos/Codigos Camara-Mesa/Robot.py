6ab88ae250d09bdedac96cdd01bb05e4a16092d7
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-07-21 00:15:20 -0600

    Update Robot.py

--- Fase 2/Codigos/Codigos Camara-Mesa/Robot.py
+++ Fase 2/Codigos/Codigos Camara-Mesa/Robot.py
@@ -225,20 +225,5 @@ class vector_robot():
         else:
             return self.Robot_vector[_id]
         
-bot = Robot()
-vector_bot = vector_robot()
-robot1 = bot.set_robot(1, 2,[0,1,3])
 
-#print(bot.get_IP())
-
-new_vector_robot = vector_bot.agregar_robot(robot1)
-#print(new_vector_robot[0][0])
-#new_vector_robot.id_robot
-
-#bot.set_IP(4)
-
-#print(bot.get_IP())
-#print(bot.get_speed())
-
-    
 
