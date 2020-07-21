beb43d06e4f80ed505d1b27b384813c39a32db2b
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-07-21 00:15:32 -0600

    Update Calibracion.py

--- Fase 2/Codigos/Codigos Camara-Mesa/Calibracion.py
+++ Fase 2/Codigos/Codigos Camara-Mesa/Calibracion.py
@@ -470,3 +470,205 @@ class camara():
                 img_counter += 1 #aumenta el contador. 
         return Cod #retorna la matriz que luego puede ser mostrada como una foto del codigo.
         
+class Robot():
+    """
+    """
+        
+    def set_robot(self, _id, _ip, _pos):
+        """
+        
+
+        Parameters
+        ----------
+        _id : TYPE
+            DESCRIPTION.
+        _ip : TYPE
+            DESCRIPTION.
+        _pos : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+        self.id_robot = _id
+        self.ip = _ip
+        self.x = _pos[0]
+        self.y = _pos[1]
+        self.theta = _pos[2]
+        self.vel_left = 0
+        self.vel_right = self.vel_left
+        self.robot = [self.id_robot,self.ip,self.x,self.y,self.theta,self.vel_right,self.vel_left]
+        return self.robot
+        
+    def set_IP(self,ip):
+        """
+        
+
+        Parameters
+        ----------
+        ip : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        ip : TYPE
+            DESCRIPTION.
+
+        """
+        self.ip = ip
+        return ip
+    
+    def set_pos(self, pos):
+        """
+        
+
+        Parameters
+        ----------
+        pos : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.x = pos[0]
+        self.y = pos[1]
+        self.theta = pos[2]
+        
+    def get_pos(self):
+        """
+        
+
+        Returns
+        -------
+        Pos : TYPE
+            DESCRIPTION.
+
+        """
+        Pos = []
+        Pos.append(self.x)
+        Pos.append(self.y)
+        Pos.append(self.theta)
+        return Pos
+        
+    def get_IP (self):
+        """
+        
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+        return self.ip
+    
+    def set_speed(self, vel):
+        """
+        
+
+        Parameters
+        ----------
+        vel : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.vel_right = vel[0]
+        self.vel_left = vel[1]
+        
+    def get_speed(self):
+        """
+        
+
+        Returns
+        -------
+        speed : TYPE
+            DESCRIPTION.
+
+        """
+        speed = []
+        speed.append(self.vel_right)
+        speed.append(self.vel_left)
+        return speed
+    
+    
+class vector_robot():
+    """
+    """
+    #robot_vector_u = Robot()
+    def __init__(self):
+        self.Robot_vector = []
+        
+    
+    def agregar_robot(self,vector_robot):
+        """
+        
+
+        Parameters
+        ----------
+        vector_robot : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+        #self.class_robot = class_robot
+        #class_robot.id_robot = self
+        #global _Robot
+        self.Robot_vector.append(vector_robot)
+        return self.Robot_vector
+    
+    def search_id_robot(self, _id):
+        """
+        
+
+        Parameters
+        ----------
+        _id : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        final_ID : TYPE
+            DESCRIPTION.
+
+        """
+        final_ID = -1
+        for i in range (0, len(self.Robot_vector)):
+            temp_Robot = self.Robot_vector[i]
+            if (temp_Robot[0] == _id):
+                final_ID = i 
+                break
+        return final_ID
+    
+    def get_robot(self, _id):
+        """
+        
+
+        Parameters
+        ----------
+        _id : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+        if _id == 0:
+            return print("No hay robot")
+        else:
+            return self.Robot_vector[_id]
+        
