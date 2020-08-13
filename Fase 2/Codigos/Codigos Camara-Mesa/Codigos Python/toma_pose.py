d7bb8f904cba99a366615dc2548824f3e744f907
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-08-12 18:51:29 -0600

    Update toma_pose.py

--- Fase 2/Codigos/Codigos Camara-Mesa/Codigos Python/toma_pose.py
+++ Fase 2/Codigos/Codigos Camara-Mesa/Codigos Python/toma_pose.py
@@ -12,6 +12,14 @@ Created on Sun Aug  9 19:21:56 2020
                              cargado de codigo el archivo de Swarm_robotic.py (ver versionado del mencionado archivo
                              para entender que hacen estas funciones y cuando son agregadas y elminadas)
                              
+12/08/2020: Version 0.3.0 -- Se agregan en la funcion getRobot_fromSnapshot() se agrega un modo para debugging.
+                             Este modo permite al usuario debugear y mostrar todas las imagenes (no recomendado
+                             en multi-hilos), mostrar solo las imagenes finales (puede usarse con multi-hilos)
+                             o no mostrar nada (el modo capture). Se arregla tambien la ubicacion del robot en la 
+                             mesa basado en las dimensiones de la calibracion (antes estaban basados en la imagen
+                             de toma de pose).
+ 
+                             
 """
 import cv2 as cv #importando libreria para opencv 
 #from Swarm_robotic import Robot
@@ -100,7 +108,7 @@ def process_image(calib_snapshot, Canny_inf, Canny_sup):
     print("voy a retonar algo")
     return contour,gray_blur_img, canny_img
 
-def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
+def getRobot_fromSnapshot(contour, snap, MyWiHe, codeSize = 3,mode = "CAPTURE"):
     """
     
 
@@ -110,21 +118,36 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
         Los contornos detectados en la foto capturada
     snap : numpy Array
         La imagen calibrada y procesada con el filtro blanco y negro asi como la difuminacion
+    MyWiHe : TYPE array 2x2
+        DESCRIPTION. El ancho y el alto (width y heigth) de la IMAGEN CALIBRADA. Al momento de calibrar
+        se debe pasar las dimensiones que se retornan para esta funcion
     codeSize : TYPE, optional
         DESCRIPTION. The default is 3. El tama;o del codigo. Usado normalmente para detectar codigos mas peque;os
         de 3x3 cm, aunque la funcionalidad ha sido probado con resultados exitos de 3x3 cm en adelante.
     mode : TYPE, optional
-        DESCRIPTION. The default is "CAPTURE". Determina el modo de operacion de esta funcion. Si es CAPTURE
-        obvia mostrar ciertas imagenes para ver si se esta detectando correctamente el codigo. Si es DEBUG, muestra 
-        estas figuras, pero esto puede interferir en el uso de los multi-hilos (ver documentacion de multi-hilos y OpenCV)
+        DESCRIPTION. The default is "CAPTURE".
+        Si mode = "CAPTURE" entonces se obvian imagenes que sirven solamente para mostrar el codigo funcionando visualmente
+        Si mode == "DEBUG" se muestran todas las figuras y otros elementos para ver el comportamiento completo del codigo
+        en los diferentes pasos de esta funcion
+        Si mode == "DEBUG_ON_CAPTURE" muestra ciertas figuras pero estas son retornadas (para usarse en multi-hilos)
+
+    Raises
+    ------
+    Exception
+        DESCRIPTION. si mode (mode de la funcion descrita en el versionado) no es adecuado tira un error.
 
     Returns
     -------
-    parameter_robot: Array
-        Retorna un array con el ID, IP y la posicion (pos[0] = x, pos[1] = y, pos[2] = theta -angulo-) para ser
-        agregado como objeto de tipo Robot (ver Swarm_robotic.py y clase ***Robot***)
+    TYPE
+        DESCRIPTION. Dependiendo el modo retorna solo los parametros del robot u otros elementos para debugging.
 
     """
+    
+    if mode == "CAPTURE" or mode == "DEBUG" or mode == "DEBUG_ON_CAPTURE":
+        pass
+    else:
+        raise Exception("Valor invalido de mode. Valores aceptados CAPTURE, DEBUG, DEBUG_ON_CAPTURE")
+
     parameter_robot = []
     for c in contour:
         #cv.drawContours(snap, c,  -1, (10,200,20), 2) #dibuja los contornos
@@ -146,7 +169,8 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
         
         #para debug, imprimi el valor del centro.
         #print(center)
-
+        if theta < -45:
+            theta += 90
         
         #separador
         print("-----------------")
@@ -173,8 +197,11 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
             
         if ctrl == 1: 
             
-            GlobalWidth = snap.shape[1]
-            GlobalHeigth = snap.shape[0]
+            GlobalWidth = MyWiHe[1]
+            GlobalHeigth = MyWiHe[0]
+            
+            MyGlobalWidth = snap.shape[1]
+            MyGlobalHeigth = snap.shape[0]
             
             #---------------------------------------------
             #separador
@@ -213,7 +240,7 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
             M = cv.getRotationMatrix2D(center, theta, 1)
             
             #se obtiene la perspectiva de la imagen basada en la matriz y las dimensiones de la imagen, esto es para recortar
-            dst = cv.warpAffine(snap, M, (GlobalWidth, GlobalHeigth)) 
+            dst = cv.warpAffine(snap, M, (MyGlobalWidth, MyGlobalHeigth)) 
             #image_rotated = cv.warpAffine(SemiCropCod, temp_matRotated, (SemiCropCod_Heigth, SemiCropCod_Width), flags = cv.INTER_CUBIC)
             
             #Final_Crop_rotated = cv.getRectSubPix(image_rotated, (int(height_cont),int(width_cont)), (np.size(rows_1)/2.0, np.size(cols_1)/2.0))
@@ -221,7 +248,9 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
             
             Final_Crop_rotated = cv.getRectSubPix(dst, size, center) #obtiene el recorte final.
             
-            
+            if mode == "DEBUG" or mode == "DEBUG_ON_CAPTURE":
+                cv.imwrite("Imagen_Inicial.png", Final_Crop_rotated) #Guarda la foro
+                initial_image = Final_Crop_rotated
             #cv.imshow("Init", SemiCropCod) 
             
 
@@ -259,7 +288,7 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
             
             #calculo del porcentaje de escala para las medidas
             height_percent = (116/height_Final_Rotated) 
-            width_percent = (116/height_Final_Rotated)
+            width_percent = (116/width_Final_Rotated)
             
             #para debug, separador e imprime el factor de escala de la imagen.
             print("-----------------")
@@ -282,6 +311,10 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
             
             # resize image
             resized = cv.resize(Final_Crop_rotated, dim, interpolation = cv.INTER_AREA)
+            
+            if mode == "DEBUG" or mode == "DEBUG_ON_CAPTURE":
+                image_befor_rotate = resized
+                cv.imwrite("Imagen_resized.png", resized) #Guarda la foro
             
             if (mode == "DEBUG"):
                 #para debug muestra la imagen recortada
@@ -393,7 +426,7 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
                #cuadro inferior derecho: 70:105, 70:105
                
             tempFloatTheta = theta #el angulo al que esta rotado el codigo. 
-            
+            print("este es el angulo de mi robot al inicio", tempFloatTheta)
     
             #comparacion mencionada, detecta cual de las esquinas tiene el mayor color para hacer la rotacion.
             if ((ColorSupDer > ColorSupIzq) and (ColorSupDer > ColorInfDer) and (ColorSupDer > ColorInfIzq)):
@@ -415,7 +448,7 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
                 resized = cv.rotate(resized, cv.ROTATE_90_CLOCKWISE)
                 tempFloatTheta = tempFloatTheta - 90
                 EscalaColores[2] = ColorInfIzq
-        
+            print("este es el angulo de mi robot rotado", tempFloatTheta)
             #temp_ColorSupIzq = Final_Crop_rotated[int(height_Final_Rotated*1/8 + 2):int(height_Final_Rotated*1/8 + 30), 10:40]
             #temp_a5 = Final_Crop_rotated[int(height_Final_Rotated*1/4 + 42):int(height_Final_Rotated*1/2 + 40), int(height_Final_Rotated*1/8):int(height_Final_Rotated*1/8 + 23)]
             #temp_a1 = Final_Crop_rotated[int(height_Final_Rotated*1/8):40, 65:100]
@@ -490,7 +523,7 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
                 #para debug, muestra los cuadros detectados
                 #--------------------------------
                 #cv.imshow("Color_a0",resized[int(height_Final_Rotated*1/8 + 2):int(height_Final_Rotated*1/8 + 30), int(height_Final_Rotated*1/8)+25:int(height_Final_Rotated*1/8)+52])
-                cv.imshow("Color_a0_2",resized[int(height_Final_Rotated*1/8 + 2):int(height_Final_Rotated*1/8 + 30), int(width_Final_Rotated*1/8)+25:int(width_Final_Rotated*1/8)+55])
+                cv.imshow("Color_a0",resized[int(height_Final_Rotated*1/8 + 2):int(height_Final_Rotated*1/8 + 30), int(width_Final_Rotated*1/8)+25:int(width_Final_Rotated*1/8)+55])
                 cv.imshow("Color_a1",resized[int(height_Final_Rotated*1/8):40, 65:100])
                 cv.imshow("Color_a2",resized[int(height_Final_Rotated*1/8)+30:int(height_Final_Rotated*1/8 + 30)+22, 12:38])
                 cv.imshow("Color_a3",resized[int(height_Final_Rotated*1/8 + 2)+23:int(height_Final_Rotated*1/8 + 25)+30, 10+30:35+30])
@@ -504,8 +537,8 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
                 cv.imshow("Codigo", resized)
                 cv.waitKey(0) 
                 #--------------------------------
-            
-            
+            if mode == "DEBUG" or mode == "DEBUG_ON_CAPTURE":
+                cv.imwrite("codigo_resized.png", resized) #Guarda la foro
                 #Generando los valores para detectar el codigo.
             temp_a3 = resized[int(height_Final_Rotated*1/8 + 2)+23:int(height_Final_Rotated*1/8 + 25)+30, 10+30:35+30]
             a3 = temp_a3[int(temp_a3.shape[0]/2),int(temp_a3.shape[1]/2)]
@@ -616,7 +649,11 @@ def getRobot_fromSnapshot(contour, snap, codeSize = 3, mode = "CAPTURE"):
     
             #En ambos return, se manda a llamar a la clase Robot para pasar sus argumentos a la clase vector_robot
             parameter_robot.append([tempID, "", pos])
-    return parameter_robot #Robot(tempID,"", pos) #si ctrl es 1, se retorna el valor correcto
+    if mode == "DEBUG_ON_CAPTURE":
+        return parameter_robot, resized, initial_image, image_befor_rotate #Robot(tempID,"", pos) #si ctrl es 1, se retorna el valor correcto
+        #return None
+    elif mode == "CAPTURE" or mode == "DEBUG":
+        return parameter_robot #Robot(tempID,"", pos) #si ctrl es 1, se retorna el valor correcto
         #return None
 
 
